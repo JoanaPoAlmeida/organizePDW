3caf9001dce85457aa5d039126f6d43648e1e53f
franciscuoliveira <58727899+franciscuoliveira@users.noreply.github.com>
2022-01-06 12:21:32 +0000

    login backend done

--- organizerBackEnd/app/Http/Controllers/UserController.php
+++ organizerBackEnd/app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTExceptions;
 
 class UserController extends Controller
 {
@@ -37,5 +39,31 @@ class UserController extends Controller
     public function login(Request $request){
         //APAGAR ESTE COMENTARIO DPS
         //MIN 24:38 https://www.youtube.com/watch?v=c2bk_Ytqhmg
+        $credentials = $request->only('email', 'password');
+        try {
+            if(!JWTAuth::attempt($credentials)){
+                $response['status'] = 0;
+                $response['code'] = 401;
+                $response['data'] = null;
+                $response['message'] = 'Email or Password is incorrect';
+                return response()->json($response);
+            }
+        } catch (JWTException $e) {
+            $response['data'] = null;
+            $response['code'] = 500;
+            $response['message'] = 'Could Not Create Token';
+            return response()->json($response);
+        }
+        $user = auth()->user();
+        $data['token'] = auth()->claims([
+            'user_id' => $user->id,
+            'email' =>$user->email
+        ])->attempt($credentials);
+
+        $response['data'] = $data;
+        $response['status'] = 1; //login successfull
+        $response['code'] = 200;
+        $response['message'] = 'Login Successfully';
+        return response()->json($response);
     }
 }
--- organizerBackEnd/config/app.php
+++ organizerBackEnd/config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
-
+        
         /*
          * Package Service Providers...
          */

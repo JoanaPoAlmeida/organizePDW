b2700eb33815837ca91ada3a43d3fe57d5d479a5
Guardanapkins <76529172+Guardanapkins@users.noreply.github.com>
2022-01-22 01:08:06 +0000

    frontend atualizado

--- FrontEnd(Angular)/src/app/gerir-categorias/gerir-categorias.component.ts
+++ FrontEnd(Angular)/src/app/gerir-categorias/gerir-categorias.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, FormControl} from '@angular/forms'
+import { Categoria } from '../model/categoria';
+import { CategoriaService } from '../service/categoria.service';
 
 @Component({
   selector: 'app-gerir-categorias',
@@ -9,21 +11,82 @@ import {FormGroup, FormBuilder, FormControl} from '@angular/forms'
 export class GerirCategoriasComponent implements OnInit {
 
   catDetail !: FormGroup
+    catObj : Categoria = new Categoria;
+    catList: Categoria[] = [];
 
-  constructor(private formBuilder : FormBuilder) { }
+
+  constructor(private formBuilder : FormBuilder, private catService : CategoriaService) { }
 
   ngOnInit(): void {
 
-    this.catDetail = this.formBuilder.group({
+    this.getAllCategories();
 
-      name : [' '],
-      description : [' ']
+    this.catDetail = this.formBuilder.group({
+      id : 1,
+      name : ' ',
+      description : ' '
     });
 
   }
 
   addCategory(){
-    console.log(this.catDetail);
+    this.catObj.id = this.catDetail.value.id;
+    this.catObj.name = this.catDetail.value.name;
+    this.catObj.description = this.catDetail.value.description;
+    
+    this.catList.push(this.catObj);
+    this.catDetail.value.id = this.catDetail.value.id+1;
+    console.log(this.catList);
+    
+   
+    this.catService.addCategory(this.catObj).subscribe(res=>{
+      console.log(res);
+      this.getAllCategories();
+    }, err=>{
+      console.log(err);
+    });
   }
 
+  getAllCategories(){
+ 
+    this.catService.getAllCategories().subscribe(res=>{
+      this.catList = res;
+    }, err=>{
+      console.log("Error while fetching the data;");
+    });
+  }
+
+  editCategory(cat: Categoria){
+
+    this.catDetail.controls['id'].setValue(cat.id);
+    this.catDetail.controls['name'].setValue(cat.name);
+    this.catDetail.controls['description'].setValue(cat.description);
+  }
+
+  deleteCategory(cat : Categoria){
+
+  }
+}*/
+import { Component, OnInit } from '@angular/core';
+
+//declare function salvar(): void;
+//declare function cancelar(): void;
+//declare function listaTabela(): void;
+//declare function adicionar(): void;
+//declare function lerDados(): void;
+//declare function validaCampos(): void;
+
+@Component({
+  selector: 'app-gerir-categorias',
+  templateUrl: './gerir-categorias.component.html',
+  styleUrls: ['./gerir-categorias.component.scss']
+})
+
+export class GerirCategoriasComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit() {
+
+  }
 }

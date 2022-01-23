e97afc2bb93056c757f373ce96337c51f243918b
JoanaPoAlmeida <58987530+JoanaPoAlmeida@users.noreply.github.com>
2022-01-23 03:26:07 +0000

    response -> request

--- organizerBackEnd/app/Http/Controllers/dashboardController.php
+++ organizerBackEnd/app/Http/Controllers/dashboardController.php
@@ -7,13 +7,13 @@ use Illuminate\Http\Request;
 class dashboardController extends Controller
 {
     //Como se distribuem as despesas num certo período de tempo ao longo das várias categorias?
-    public function dashboardAllDespesas(Response $response){
+    public function dashboardAllDespesas(Request $request){
         
         
     }
 
     //Qual foi a evolução das despesas de uma certa categoria ao longo de um determinado período de tempo?
-    public function dashboardDespesasByCategoria(Response $response){
+    public function dashboardDespesasByCategoria(Request $request){
 
     }
 }

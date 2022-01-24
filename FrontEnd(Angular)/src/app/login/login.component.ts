33156acc7e426367e1065a1593177d3c6ae10701
Guardanapkins <76529172+Guardanapkins@users.noreply.github.com>
2022-01-24 00:13:05 +0000

    login

--- FrontEnd(Angular)/src/app/login/login.component.ts
+++ FrontEnd(Angular)/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { LoginService } from '../service/login.service';
+import { User } from '../model/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginDetail !: FormGroup
+  loginObj : User = new User;
+  loginList: User[] = [];
+
+  constructor(private formBuilder : FormBuilder, private loginService : LoginService, public router: Router) { }
 
   ngOnInit(): void {
+
+    this.loginDetail = this.formBuilder.group({
+      username : [''],
+      password: ['']
+    });
   }
 
+
+  loginconfirmation(){
+    this.loginObj.name= this.loginDetail.value.username;
+    this.loginObj.password= this.loginDetail.value.password;
+    
+    this.loginService.confirmLogin(this.loginObj);
+  }
 }
---
+++ FrontEnd(Angular)/src/app/service/login.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  loginURL : string;
+  
+  constructor(private http: HttpClient, public router: Router) { 
+    this.loginURL = 'http://127.0.0.1:8000/api/login??'
+  }
+
+  confirmLogin(user: User){
+    return this.http.post<User>(this.loginURL, user).subscribe((res: any)=>{
+      if(res=="Login falhou"){
+        alert(res);
+      }else{
+        this.router.navigate(['/mainpage/dashboard']);
+      }
+    });
+
+  }
+}
